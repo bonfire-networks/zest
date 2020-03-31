94639724be71b1d6c4776b733f131506506cda35
James Laver <james.laver@gmail.com>
2020-03-31 11:31:15 +0200

    Release v0.1.1 because apparently i buggered the first release by not cleaning my WIP out

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Zest.MixProject do
   def project do
     [
       app: :zest,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       package: [

0155c1dfccb651b4ea17a6d08d13b58c70ebb52e
Mayel de Borniol <git@mayel.space>
2022-07-05 11:19:14 +1200

    urls

--- mix.exs
+++ mix.exs
@@ -1,13 +1,13 @@
 # Zest - a fresh approach to testing
 #
 # Copyright (c) 2020 James Laver
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,14 +26,14 @@ defmodule Zest.MixProject do
         description: "A fresh approach to testing",
         licenses: ["Apache 2"],
         links: %{
-          "Repository" => "https://github.com/commonspub/zest",
+          "Repository" => "https://github.com/bonfire-networks/zest",
           "Hexdocs" => "https://hexdocs.pm/zest"
         }
       ],
       docs: [
         name: "Zest",
         main: "readme",
-        source_url: "https://github.com/commonspub/zest",
+        source_url: "https://github.com/bonfire-networks/zest",
         extras: [
           "README.md",
           "CONDUCT.md"
